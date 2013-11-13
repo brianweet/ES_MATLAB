7a727e36349a82f9d1e50e5e1a382f632ff452bd
brianweet <brian_weeteling@hotmail.com>
2013-11-13 16:50:03 +0100

    Updated the code after the lecture (understanding the theory a bit better)

--- es.m
+++ es.m
@@ -1,12 +1,14 @@
 function [xp, fp, stat] = es(fitnessfct, n, lb, ub, stopeval)
 % [xp, fp, stat] = es(fitnessfct, n, lb, ub, stopeval)
   % Strategy parameters
-  ...
-  tau = 1/sqrt(n);
+  successRate = 0;
+  c = 0.817; % 0.817 <= c <= 1
+  
   % Initialize
   xp = rand(1,n);
   fp = feval(fitnessfct,xp);
-  sigma = (lb + (ub-lb).* rand(1,1) ) / 6;
+  %TODO BwE:  check the sigma initialization
+  sigma = (lb + (ub-lb).* rand(1,1) ) / 6; 
   evalcount = 0;
 
   % Statistics administration
@@ -18,18 +20,30 @@ function [xp, fp, stat] = es(fitnessfct, n, lb, ub, stopeval)
   % Evolution cycle
   while evalcount < stopeval
     % Generate offspring and evaluate
-    sigmao = sigma * exp(tau*randn);
-    xo = xp + sigmao * randn(1, n); % generate offspring from parent xp 
-    fo = feval(fitnessfct,xo); % evaluate xo using fitnessfct
+    % generate offspring from parent xp 
+    xo = xp + sigma * randn(1, n);
+    % evaluate xo using fitnessfct
+    fo = feval(fitnessfct,xo); 
     evalcount = evalcount + 1;
 
-    % select best and update success-rate and update stepsize
-    % Important: MINIMIZATION!
+    % select best and update success-rate
     if(fo < fp)
+        successRate = successRate + 1;
         fp = fo;
         xp = xo;
-        sigma = sigmao;
     end
+    
+    %update stepsize: change sigma every 5th execution (if needed)
+    if(mod(evalcount,5) == 0)
+        if(successRate > 2)
+            sigma = sigma / c;
+        else if(successRate < 2)
+                sigma = sigma * c;
+            end
+        end
+        successRate = 0;
+    end
+        
     
     % Statistics administration
     stat.histsigma(evalcount) = sigma;% stepsize history
--- es_test.m
+++ es_test.m
@@ -1,5 +1,13 @@
-%es(@sphere, 10, -10, 10, 10000)
+es(@sphere, 10, -10, 10, 10000)
 
-multiple_runs(@sphere, 10, -10, 10, 10000, 20)
-load('statistics.mat');
-plot_statistics(stat, @sphere, 10, -10, 10, 10000, 20)
+% multiple_runs(@sphere, 10, -10, 10, 10000, 10)
+% load('statistics.mat');
+% plot_statistics(stat, @sphere, 10, -10, 10, 10000, 10)
+
+% multiple_runs(@ackley, 10, -10, 10, 10000, 10)
+% load('statistics.mat');
+% plot_statistics(stat, @ackley, 10, -10, 10, 10000, 10)
+
+% multiple_runs(@rosenbrock, 10, -10, 10, 10000, 10)
+% load('statistics.mat');
+% plot_statistics(stat, @rosenbrock, 10, -10, 10, 10000, 10)
