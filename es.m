18be76a2601ed1ef9e64994ab5b985b786593b22
brianweet <brian_weeteling@hotmail.com>
2013-12-04 16:41:04 +0100

    Discrete and intermediate recombination. Random, proportional and tournament selection

--- es.m
+++ es.m
@@ -11,7 +11,7 @@ function [xp, fp, stat] = es(fitnessfct, n, lb, ub, stopeval)
   
     % Strategy parameters
     % Amount of parent individuals
-    mu = 2;
+    mu = 5;
     % Amount of child individuals
     lambda = 15;
   
@@ -19,6 +19,7 @@ function [xp, fp, stat] = es(fitnessfct, n, lb, ub, stopeval)
     xp = initialize_population(n, mu, lb(1,1), ub(1,1));
     % Create variable for fitness of the population
     fp = zeros(mu,1);
+    
     % Initialize one sigma randomly between upper and lowerbound / 6
     % TODO BwE:  check the sigma initialization
     sigma = (lb(1,1) + (ub(1,1)-lb(1,1))* rand(1,1)) / 6;
@@ -35,6 +36,13 @@ function [xp, fp, stat] = es(fitnessfct, n, lb, ub, stopeval)
     stat.histsigma = zeros(1, stopeval);
     stat.histf = zeros(1, stopeval);
     
+    % Initialize fitness
+    for i = 1:mu
+        fp(i,1) = feval(fitnessfct,xp(i,:));
+    end
+    
+    evalcount = evalcount + mu;
+    
     % Evolution cycle
     while evalcount < stopeval
         % Initialize memory for offspring 
@@ -49,8 +57,27 @@ function [xp, fp, stat] = es(fitnessfct, n, lb, ub, stopeval)
         
         for i = 1:lambda
             % Step 1: Recombination 
-            [r1, r2] = selection(xp,fp);
-            offspring(i) = recombine_discrete(r1,r2);
+
+            %TODO BwE: create setting for recombination  method
+            
+            %intermediate with two parents   
+            r1 = select_random(xp);
+            r2 = select_random(xp);  
+            offspring(i,:) = recombine_intermediate([r1;r2]);
+
+%             %discrete with two parents
+%             r1 = select_random(xp);
+%             r2 = select_random(xp);            
+%             offspring(i,:) = recombine_discrete([r1;r2]);
+%
+%             %intermediate with all parents   
+%             r1 = select_random(xp);
+%             r2 = select_random(xp); 
+%             offspring(i,:) = recombine_intermediate(xp);
+%             
+%             %discrete with all parents          
+%             offspring(i,:) = recombine_discrete(xp);
+            
             % Step 2: Mutation
             offspring(i) = mutate(offspring(i));
         end 
@@ -96,36 +123,100 @@ function [xp, fp, stat] = es(fitnessfct, n, lb, ub, stopeval)
 
 end
 
-function xp = initialize_population(n, mu, lower, upper)
+function population = initialize_population(n, mu, lowerbound, upperbound)
     % xp = initialize_population(n, mu)
     %
     %   Initialize new population with values within a given range
     %   n is the number of dimensions
     %   mu is the number of individuals
-    %   lower is the lowerbound
-    %   upper is the upperbound
+    %   lowerbound is the lowerbound
+    %   upperbound is the upperbound
     %
     % Author: B. Weeteling
-    xp = lower+(upper-lower).*rand(mu,n);
+    population = lowerbound+(upperbound-lowerbound).*rand(mu,n);
 end
 
-function recombined_offspring = recombine_discrete(first_parent,second_parent)
-    % recombined_individual = recombine_discrete(first_parent,second_parent)
-    %
-    %   Recombine two parents to create one offspring
-    %   first_parent is one of the parents that will be recombined
-    %   second_parent is one of the parents that will be recombined
-    %   The order of first_parent and second_parent is arbitrary
+%Recombination methods
+function recombined_offspring = recombine_discrete(population)
+    % recombined_offspring = recombine_discrete(population)
+    %   
+    %   Can be used for discrete and global discrete recombination 
+    %   population is the list of the parents that will be recombined
+    %   The order of population is arbitrary
+    %   The size of population should be > 1
     %
     % Author: B. Weeteling
     
-    % Create random string (prob 0.5 for either 0 or 1). E.g. 111000
-    rnd1 = rand(n,1) > 0.5;        
-    % Select bits from first parent where rnd1 == 1. E.g. 101110 + 111000 -> 101000
-    first_parent = and(first_parent,rnd1);
-    % Select bits from first parent where rnd1 == 0  E.g. 111111 + 000111 -> 000111
-    second_parent = and(second_parent,~rnd1);
-    % Do the actual recombination E.g. 101000 + 000111 -> 101111
-    recombined_offspring = first_parent|second_parent;
+    population_size = size(population,1);
+    length_of_individual = size(population,2);
+    selected_positions = randi(population_size,length_of_individual,1);
+    
+    recombined_offspring = zeros(length_of_individual,1);
+    
+    for position_index = 1:length_of_individual
+        selected_parent = population(selected_positions(position_index),:);
+        recombined_offspring(position_index) = selected_parent(1,position_index);
+    end
+end
+
+function recombined_offspring = recombine_intermediate(population)
+    % recombined_offspring = recombine_intermediate(population)
+    %   
+    %   Can be used for intermediate and global intermediate recombination 
+    %   population is the list of the parents that will be recombined
+    %   The order of population is arbitrary
+    %   The size of population should be > 1
+    %
+    % Author: B. Weeteling
+    
+    recombined_offspring = mean(population);
+end
+
+%Selection methods
+function selected_individual = select_random(population)
+% selected_individual = select_random(population)
+%
+%   Select 1 individual of population 'population'
+%
+% Author: B. Weeteling
+length_of_individual = size(population,1);
+random_individual_idx = randi(length_of_individual,1,1);
+selected_individual = population(random_individual_idx,:);
+end
+
+function selected_individual = select_proportional(population, population_fitness)
+% selected_individual = select_proportional(parents, fitness)
+%
+%   Select 1 individual of population 'population' with fitness
+%   values 'population_fitness' using proportional selection.
+%
+% Author: Johannes W. Kruisselbrink
+% Last modified: October 21, 2009
+
+	cumsum_f = cumsum(population_fitness);
+	r = sum(population_fitness) * rand();
+	i = 1;
+	while (r >= cumsum_f(i))
+		i = i + 1;
+	end
+	selected_individual = population(i, :);
+
+end
+
+function selected_individual = select_tournament(population, population_fitness, tournament_size)
+% selected_individual = select_tournament(population, population_fitness, tournament_size)
+%
+%   Select 1 individual of population 'population' with fitness
+%   values 'population_fitness' using tournament selection.
+%   'tournament_size' is the tournament size
+%
+% Author: B. Weeteling
+
+    selected_individuals_index = randi(size(population,1),1,tournament_size);
+    selected_individuals = population(selected_individuals_index,:);
+    selected_fitness = population_fitness(selected_individuals_index,:);
+    
+    [~,best_index] = sort(selected_fitness);
+    selected_individual = selected_individuals(best_index(1,1), :);
 end
 
